247081a0f4bf5e970bbb29b79e69a17bd98a0bb8
Tolga Paksoy <tlgpksy@gmail.com>
2014-12-18 15:43:01 +0100

    use variable partition size

--- src/main/scala/Mnist.scala
+++ src/main/scala/Mnist.scala
@@ -4,13 +4,14 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object Mnist extends App {
   private final val SEPERATOR = "\t"
+  private final val SPLITS    = 28
 
   val conf = new SparkConf().setAppName("MNIST Neural Network")
   val sc = new SparkContext(conf)
 
-  val trainImages = sc.textFile(args(0), 8).cache
+  val trainImages = sc.textFile(args(0), SPLITS).cache
       .map(_ split SEPERATOR)
-  val testImages  = sc.textFile(args(1), 8).cache
+  val testImages  = sc.textFile(args(1), SPLITS).cache
       .map(_ split SEPERATOR)
 
   val trainValues = trainImages.map(e => Vectors.dense(e.tail.map(_.toDouble))).cache
