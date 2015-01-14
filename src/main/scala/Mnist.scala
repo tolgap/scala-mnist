4bbd5c543294f575ccf3a1dd5fd6e8faa5edcd27
Tolga Paksoy <tlgpksy@gmail.com>
2015-01-14 10:12:19 +0100

    fixed caching and added logging

--- src/main/scala/Mnist.scala
+++ src/main/scala/Mnist.scala
@@ -9,10 +9,10 @@ object Mnist extends App {
   val conf = new SparkConf().setAppName("MNIST Neural Network")
   val sc = new SparkContext(conf)
 
-  val trainImages = sc.textFile(args(0), SPLITS).cache
-      .map(_ split SEPERATOR)
-  val testImages  = sc.textFile(args(1), SPLITS).cache
-      .map(_ split SEPERATOR)
+  val trainImages = sc.textFile(args(0), SPLITS)
+      .map(_ split SEPERATOR).cache
+  val testImages  = sc.textFile(args(1), SPLITS)
+      .map(_ split SEPERATOR).cache
 
   val trainValues = trainImages.map(e => Vectors.dense(e.tail.map(_.toDouble))).cache
   val trainLabels = trainImages.map {
@@ -22,16 +22,21 @@ object Mnist extends App {
       Vectors.dense(labs)
     }
   }.cache
-  val train = trainValues.zip(trainLabels.cache).cache
+  val train = trainValues.zip(trainLabels).cache
   val testValues = testImages.map(e => Vectors.dense(e.tail.map(_.toDouble))).cache
   val testLabels = testImages.map(e => Vectors.dense(e.last.toDouble)).cache
 
+  val startTime = System.currentTimeMillis
   val network = ArtificialNeuralNetwork.train(train, Array[Int](300), 1000)
+  val endTime = System.currentTimeMillis
 
-  val prediction = network.predict(testValues.cache).map(_._2.toArray).cache
-  val pred = prediction.map(indexOfMax).cache
-  val output = pred.zip(testLabels)
-  output.saveAsTextFile(args(2))
+  val prediction = network.predict(testValues).map(_._2.toArray).cache
+  val pred       = prediction.map(indexOfMax).cache
+  val output     = pred.zip(testLabels)
+  val errRate    = output.map(T => (T._2.toArray(0) - T._1) * (T._2.toArray(0) - T._1)).reduce((u,v) => u + v)
+//  output.saveAsTextFile(args(2))
+
+  println("Elapsed training time: ${(endTime - startTime) / 1000}s. Error rate: ${errRate}.")
 
   final def indexOfMax(values: Array[Double]) = {
     val max = values.max
