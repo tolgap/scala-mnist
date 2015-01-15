b57185ed6217aeaff5873e77dab114305d06f51a
Tolga Paksoy <tlgpksy@gmail.com>
2015-01-15 10:06:10 +0100

    improve error rate handling by refactor

--- src/main/scala/Mnist.scala
+++ src/main/scala/Mnist.scala
@@ -1,46 +1,51 @@
 import org.apache.spark.mllib.ann.ArtificialNeuralNetwork
-import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 
 object Mnist extends App {
-  private final val SEPERATOR = "\t"
+  private val SEPERATOR = "\t"
+  private val SPLITS    = 24
 
   val conf = new SparkConf().setAppName("MNIST Neural Network")
   val sc = new SparkContext(conf)
 
-  val trainImages = sc.textFile(args(0))
+  val trainImages = sc.textFile(args(0), SPLITS)
       .map(_ split SEPERATOR).cache
-  val testImages  = sc.textFile(args(1))
+  val testImages  = sc.textFile(args(1), SPLITS)
       .map(_ split SEPERATOR).cache
 
-  val trainValues = trainImages
-      .map(e => Vectors.dense(e.init.map(_.toDouble))).cache
-  val trainLabels = trainImages.map {
-    e => {
-      val labs = Array.ofDim[Double](10)
-      labs(e.last.toInt) = 1
-      Vectors.dense(labs)
-    }
-  }.cache
+  val (trainValues, trainLabels) = splitValuesLabels(trainImages)
   val train = trainValues.zip(trainLabels).cache
-  val testValues = testImages.map(e => Vectors.dense(e.tail.map(_.toDouble))).cache
-  val testLabels = testImages.map(e => Vectors.dense(e.last.toDouble)).cache
+
+  val (testValues, testLabels) = splitValuesLabels(testImages)
 
   val startTime = System.currentTimeMillis
-  val network = ArtificialNeuralNetwork.train(train, Array[Int](300), 1000)
-  val endTime = System.currentTimeMillis
+  val network   = ArtificialNeuralNetwork.train(train, Array[Int](300), 1000)
+  val endTime   = System.currentTimeMillis
 
-  val prediction = network.predict(testValues).map(_._2.toArray).cache
-  val pred       = prediction.map(indexOfMax).cache
-  val output     = pred.zip(testLabels)
-  val errRate    = output.map(T => (T._2.toArray(0) - T._1) * (T._2.toArray(0) - T._1))
-      .reduce((u,v) => u + v)
+  val prediction = network.predict(testValues).map(_._2).cache
+  val output     = prediction.zip(testLabels).cache
+  val errRate    = output.map(T =>
+      (T._2.toArray(0) - T._1.toArray(0)) * (T._2.toArray(0) - T._1.toArray(0))
+    ).reduce((u, v) => u + v)
 //  output.saveAsTextFile(args(2))
 
   println(s"Elapsed training time: ${(endTime - startTime) / 1000}s. Error rate: $errRate.")
 
-  final def indexOfMax(values: Array[Double]) = {
+  def indexOfMax(values: Array[Double]) = {
     val max = values.max
-    values.indexOf(max)
+    values indexOf max
+  }
+
+  def splitValuesLabels(valLab: RDD[Array[String]]): (RDD[Vector], RDD[Vector]) = {
+    val values = valLab.map(e => Vectors.dense(e.init.map(_.toDouble)))
+    val labels = valLab.map {
+      e =>
+        val labs = Array.ofDim[Double](10)
+        labs(e.last.toInt) = 1D
+        Vectors.dense(labs)
+    }
+    (values.cache, labels.cache)
   }
 }
