6ab1011ee01b0e2743a6e74f1394ba0390f27f28
Tolga Paksoy <tlgpksy@gmail.com>
2015-01-19 16:12:16 +0100

    reduce the number of hidden neurons and max iters

--- src/main/scala/Mnist.scala
+++ src/main/scala/Mnist.scala
@@ -21,7 +21,7 @@ object Mnist extends App {
   val (testValues, testLabels) = splitValuesLabels(testImages)
 
   val startTime = System.currentTimeMillis
-  val network   = ArtificialNeuralNetwork.train(train, Array[Int](300), 500)
+  val network   = ArtificialNeuralNetwork.train(train, Array[Int](100), 100)
   val endTime   = System.currentTimeMillis
 
   val prediction = network.predict(testValues).map(_._2).cache
